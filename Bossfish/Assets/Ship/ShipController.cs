a16262f81bfa9e4f674cee2868d02e3a2d8d6f84
taBooRet <levp3437@gmail.com>
2026-03-04 14:17:51 +0200

    Add-shipController

--- Bossfish/Assets/Ship/ShipController.cs
+++ Bossfish/Assets/Ship/ShipController.cs
@@ -1,6 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class ShipFollower : MonoBehaviour
+public class ShipController : MonoBehaviour
 {
-    
+    public Transform enginePoint;
+    public Rigidbody rb;
+    public WaterSystems waterSystems;
+    public float speed;
+    public Vector2 input;
+
+    private void FixedUpdate()
+    {
+        if (input != Vector2.zero)
+        {
+            float waterLevel = waterSystems.GetWaterLevel(enginePoint.position);
+            if (waterLevel - enginePoint.position.y > 0)
+            {
+                Vector3 direction = transform.right * input.x;
+                direction += transform.forward * input.y;
+                rb.AddForceAtPosition(direction * speed, enginePoint.position);
+            }
+        }
+    }
+
 }
