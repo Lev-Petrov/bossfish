b5ca823a520d644d2be21be85630dd5c2a065776
taBooRet <levp3437@gmail.com>
2026-03-06 21:09:02 +0200

    fix-merg

--- Bossfish/Assets/Ship/VirtualShip.cs
+++ Bossfish/Assets/Ship/VirtualShip.cs
@@ -1,17 +1,51 @@
+﻿
 
 using UnityEngine;
 using System.Collections.Generic;
 
 public class VirtualShip : MonoBehaviour
 {
-    public Rigidbody rb; // is kinematic
+    public Rigidbody rb;
     public Rigidbody ship;
     [HideInInspector] public List<Rigidbody> onTheBoard = new List<Rigidbody>();
+
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
+    private void Start()
+    {
+        lastPosition = ship.position;
+        lastRotation = ship.rotation;
+    }
 
     private void FixedUpdate()
     {
         rb.position = ship.position;
         rb.rotation = ship.rotation;
+        rb.position = lastPosition;
+        rb.rotation = lastRotation;
+
+        Vector3 deltaPosition = ship.position - lastPosition;
+        Quaternion deltaRotation = ship.rotation * Quaternion.Inverse(lastRotation);
+
+        foreach (Rigidbody rb in onTheBoard)
+        {
+            if (rb == null) continue;
+
+            // ���������� ����
+            rb.MovePosition(rb.position + deltaPosition);
+
+            // ���������
+            Vector3 dir = rb.position - ship.position;
+            dir = deltaRotation * dir;
+            Vector3 newPos = ship.position + dir;
+
+            rb.MovePosition(newPos);
+            rb.MoveRotation(deltaRotation * rb.rotation);
+        }
+
+        lastPosition = ship.position;
+        lastRotation = ship.rotation;
     }
 
     //Add object on the board
@@ -19,6 +53,9 @@ public class VirtualShip : MonoBehaviour
     {
         other.transform.SetParent(transform);
         onTheBoard.Add(other.GetComponent<Rigidbody>());
+        Rigidbody rb = other.attachedRigidbody;
+        if (rb != null && !onTheBoard.Contains(rb))
+            onTheBoard.Add(rb);
     }
 
     //Remove object from the board
@@ -26,6 +63,9 @@ public class VirtualShip : MonoBehaviour
     {
         other.transform.SetParent(null);
         onTheBoard.Remove(other.gameObject.GetComponent<Rigidbody>());
+        Rigidbody rb = other.attachedRigidbody;
+        if (rb != null)
+            onTheBoard.Remove(rb);
     }
 
     //Apply object's weight on the real ship
